c982d8ee5f68f73ee60b5de47bbe1d88d0121638
wak6817 <b.morris.claus@icloud.com>
2026-08-11 15:47:07 +0200

    added new selectors

--- build/build-colorv.sh
+++ build/build-colorv.sh
@@ -5,5 +5,6 @@ ROOT="$(cd "$(dirname "$0")/.." && pwd)"
 cat "$ROOT/src/colorv/interactions.css" \
     "$ROOT/src/colorv/other.css" \
     "$ROOT/src/colorv/text.css" \
+    "$ROOT/src/color-trans.css" \
     | lua "$ROOT/build/add-important.lua" \
     > "$ROOT/build/colorv.css"
