e4cac8755c9f40e7f6b17900889f42606868d8f8
wak6817 <b.morris.claus@icloud.com>
2026-08-11 10:29:55 +0200

    made run.lua easier to use

--- build/build-colorv.sh
+++ build/build-colorv.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-cat "$ROOT/src/colorv/colors.css" \
-    "$ROOT/src/colorv/interactions.css" \
+cat "$ROOT/src/colorv/interactions.css" \
     "$ROOT/src/colorv/other.css" \
     "$ROOT/src/colorv/text.css" \
     | lua "$ROOT/build/add-important.lua" \
--- run.lua
+++ run.lua
@@ -1,13 +1,26 @@
 function main()
     print("color version or full version")
-    local input = io.read()
+    local version = io.read()
 
-    if input == "color version" then
+    if version == "color version" then
         os.execute("sh /build/colorv.sh")
-    elseif input == "full version" then
+    elseif version == "full version" then
         os.execute("sh /build/fullv.sh")
     else
+        print("write color version or full version")
         main()
+    end
+
+    print("CSS file generated in /build/")
+    print("go to /build/ (y/n)")
+    local gotobuild = io.read() -- dont rename to goto-build
+
+    if gotobuild == "y" then
+        os.execute("cd build && ls")
+    elseif gotobuild == "n" then
+        print("ok script done")
+    else
+        print("defaulted to no")
     end
 end
 
